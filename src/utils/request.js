97884c7f4540f4136dc3c44590db2690d4e95272
lianyu <1319764245@qq.com>
2024-03-22 16:25:14 +0800

    配置请求和响应拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
 
+import store from '@/store'
+import router from '@/router'
+import { Message } from 'element-ui'
+
 const instance = axios.create({
   baseURL: 'http://interview-api-t.itheima.net',
   timeout: 3000
@@ -9,6 +13,10 @@ const instance = axios.create({
 instance.interceptors.request.use(
   function (config) {
     // 在发送请求之前做些什么
+    const token = store.state.user.token
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
     return config
   },
   function (error) {
@@ -27,6 +35,15 @@ instance.interceptors.response.use(
   function (error) {
     // 超出 2xx 范围的状态码都会触发该函数。
     // 对响应错误做点什么
+    if (error.response) {
+      // 401 登录失效
+      if (error.response.data.code == 401) {
+        Message.error('登录失效，请重新登录')
+        router.push('/login')
+      } else {
+        Message.error(error.response.data.message)
+      }
+    }
     return Promise.reject(error)
   }
 )
