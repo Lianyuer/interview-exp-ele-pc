8c0d6e94750ee723c0af497a767858707a3f312e
lianyu <1319764245@qq.com>
2024-03-22 10:49:09 +0800

    配置 eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['plugin:vue/essential', 'eslint:recommended', 'plugin:prettier/recommended'],
+  extends: ['plugin:vue/essential', 'eslint:recommended', 'plugin:prettier/recommended'], // 启用 Prettier 的推荐规则，与 Prettier 一起使用时，这些规则可以确保代码格式与 Prettier 保持一致
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
@@ -11,6 +11,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'prettier/prettier': 'error'
+    'prettier/prettier': 'error' // 如果代码不符合 Prettier 的格式要求，则报错
   }
 }
